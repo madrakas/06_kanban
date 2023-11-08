6f21e84ee0e176fcd4114354518df8f8a90e6aa1
Arvydas <arvydas.simbelis@gmail.com>
2023-11-09 00:17:03 +0200

    Kanban add delete tasks

--- js/Todo.js
+++ js/Todo.js
@@ -3,6 +3,9 @@ export class Todo {
         this.selector = selector;
         this.columns = columns;
         this.DOM = null;
+        this.columnsDOM = [];
+        this.tasks = [];
+        this.lastUsedtaskID = 0;
 
         this.init();
     }
@@ -28,18 +31,64 @@ export class Todo {
 
         for (const column of this.columns) {
             HTML += `
-            <div>
-                <h2>${column}</h2>
-                <ul>
-                    <li>Task</li>
-                    <li>Task</li>
-                    <li>Task</li>
-                    <li>Task</li>
-                </ul>
+            <div class="column">
+                <h2 class="title">${column}</h2>
+                <ul class="task-list">
+     
+                 </ul>
             </div>`;
         }
+
         this.DOM.classList.add('todo');
         this.DOM.innerHTML = HTML;
         this.DOM.style.gridTemplateColumns = `repeat(${this.columns.length}, 1fr)`;
+        this.columnsDOM = this.DOM.querySelectorAll('.task-list');
+        // console.log(this.columnsDOM);
     }
+
+    addTask(task){
+        this.tasks.push({
+            ...task,
+            isDeleted: false,
+        });
+
+        const taskID = ++this.lastUsedtaskID;
+        let tagsHTML = '';
+        let bgcolor = '#cccccc'
+
+        for (const tag of task.tags){
+            if (tag.color.length < 7 ){
+                bgcolor = tag.color + '1';
+            }else {
+                bgcolor = tag.color + '11';
+            }
+            tagsHTML += `<div class="tag" style="color: ${tag.color}; background-color: ${bgcolor}">${tag.text}</div>`;
+        }
+
+        const HTML = 
+        `<li id="task_${taskID}" class="task-card">
+        <div class="task-actions">
+            <button class="fa fa-trash"></button>
+        </div>
+        <div class="task-title">${task.title}</div>
+        <div class="task-desc">${task.desc}</div>
+        <div class="task-tags">${tagsHTML}</div>
+        <div class="task-deadline">${task.deadline}</div>
+        </li>`
+        
+       // this.columnsDOM[task.columnIndex].innerHTML += HTML;
+         this.columnsDOM[task.columnIndex].insertAdjacentHTML('beforeend', HTML);
+       
+        const taskDOM = document.getElementById(`task_${taskID}`);
+        const deleteButtonDOM = taskDOM.querySelector('.fa-trash');
+        
+        deleteButtonDOM.addEventListener('click', () => {
+            // console.log('delete', taskDOM);
+            this.tasks[taskID - 1].isDeleted = true;
+            taskDOM.remove();
+            console.log(taskID);
+        });
+    };
  };
+
+ 
--- js/main.js
+++ js/main.js
@@ -5,4 +5,38 @@ const kanban = new Todo(
     ['Backlog','Todo', 'In progress', 'Done']
     );
 
-    // kanban.addTask('');
+    kanban.addTask({
+        columnIndex: 0,
+        title: 'Pirma užduotis',
+        desc: 'Pirm. užd. aprašas  dfslkgdflg ;dlsdfgh ;dsafh g;lasdh ;laksdfl hsad;fh sadf',
+        createdOn: '2023-11-08 09:03:15',
+        deadline: '2023-12-25 00:00:00',
+        tags: [
+            {text: 'Design', color: '#333'}
+        ],
+    });
+
+    kanban.addTask({
+        columnIndex: 1,
+        title: 'Antra užduotis',
+        desc: 'Antra užd. aprašas  dfslkgdflg ;dlsdfgh ;dsafh g;lasdh ;laksdfl hsad;fh sadf',
+        createdOn: '2023-11-08 09:03:15',
+        deadline: '2023-12-25 00:00:00',
+        tags: [
+            {text: 'UX', color: '#f00'},
+            {text: 'UI', color: '#090'},
+        ],
+    });
+
+    kanban.addTask({
+        columnIndex: 0,
+        title: 'Trečia užduotis',
+        desc: 'Trečia užd. aprašas  dfslkgdflg ;dlsdfgh ;dsafh g;lasdh ;laksdfl hsad;fh sadf',
+        createdOn: '2023-11-08 09:03:15',
+        deadline: '2023-12-25 00:00:00',
+        tags: [
+            {text: 'Development', color: '#00c'}
+        ],
+    });
+
+    console.log(kanban);
